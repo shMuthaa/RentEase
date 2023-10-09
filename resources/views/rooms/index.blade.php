1890b41952755f07b79481d8fb7fcace56416d44
STANLEY NGUGI <leeblittz@gmail.com>
2023-10-09 14:28:56 +0300

    Nulled

--- resources/views/rooms/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.dashboard')
-
-@section('content')
-    <main>
-        <div class="dashboard">
-            <h1>Rooms</h1>
-        </div>
-    </main>
-@endsection
--- resources/views/tenants/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.dashboard')
-
-@section('content')
-    <main>
-        <div class="dashboard">
-            <h1>Tenants</h1>
-        </div>
-    </main>
-@endsection
